80416035e0b9f951a37762bd31bb37d30363765d
Eric Chang <efrochang@gmail.com>
2018-04-18 00:03:41 -0500

    Add JQuery and Bootstrap

--- src/main/java/com/emma/AwsApplication.java
+++ src/main/java/com/emma/AwsApplication.java
@@ -1,4 +1,4 @@
-package com.aws;
+package com.emma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/emma/AwsApplicationTests.java
+++ src/test/java/com/emma/AwsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.aws;
+package com.emma;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
